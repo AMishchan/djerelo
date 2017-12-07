28345dc1ae05b05bd0de59500f9ff2a43c9daeb5
Alex Kox <ua.web.zona@gmail.com>
2017-12-07 12:22:26 +0200

    lang messages hello added

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serf
+ * Date: 12/7/17
+ * Time: 12:19 PM
+ */
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'welcome' => 'Hello.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
